1c4cc33d4c0fbea188478de739996194c68f744f
Erin Woo <erinpwoo@yahoo.com>
2017-07-23 20:40:19 -0700

    Initial Commit

---
+++ FoodarFeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodarFeed
+//
+//  Created by Erin Woo on 7/23/17.
+//  Copyright © 2017 Erin Woo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
