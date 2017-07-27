bcc81e605d9515609f40ca2c213b544a7485978a
Erin Woo <erinpwoo@yahoo.com>
2017-07-27 12:25:38 -0700

    Added FoodarHeader.swift

--- FoodarFeed/FeedViewController.swift
+++ FoodarFeed/FeedViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FeedViewController: UITableViewController {
 
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
 
 
 }
+
+
+
 
