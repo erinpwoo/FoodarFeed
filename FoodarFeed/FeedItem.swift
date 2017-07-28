2c57c8e10ae4636b694eaa572bb4d23e297cc784
Erin Woo <erinpwoo@yahoo.com>
2017-07-27 21:16:25 -0700

    Created FeedItem objects and FeedItem attributes
    Also implemented new functions and methods for initializing and adding
    new FeedItem objects to the photoList array

--- FoodarFeed/FeedItem.swift
+++ FoodarFeed/FeedItem.swift
@@ -7,10 +7,26 @@
 //
 
 import Foundation
+import UIKit
 
 class FeedItem {
-    var photo = ""
-    var itemName = ""
-    var restaurantName = ""
-    var craves: Int = 0
+    var photo = UIImage()
+    let itemName: String
+    let restaurantName: String
+    var craves = Int()
+    
+    init(photo: UIImage, itemName: String, restaurantName:String, craves: Int) {
+        self.photo = photo
+        self.itemName = itemName
+        self.restaurantName = restaurantName
+        self.craves = craves
+    }
+    
+    init() {
+        self.itemName = "Item Name"
+        self.restaurantName = "Restaurant Name"
+        self.craves = 0
+    }
+    
+    
 }
--- FoodarFeed/FeedViewController.swift
+++ FoodarFeed/FeedViewController.swift
@@ -16,11 +16,17 @@ class FeedViewController: UITableViewController {
     
     
     //Placeholder array of photos
-    var photoList = [UIImage(named: "burger")!,
-                    UIImage(named: "pho")!,
-                    UIImage(named: "salad")!,
-                    UIImage(named: "icecream")!,
-                    UIImage(named: "sushirrito")]
+    var photoList: Array<FeedItem> =  Array()
+    
+    
+    // manually initializing FeedItem objects *for testing purposes*
+    let burger = FeedItem(photo: (UIImage(named: "burger"))!, itemName: "burger", restaurantName: "super duper", craves: 219)
+    let pho = FeedItem(photo: (UIImage(named: "pho"))!, itemName: "veggie pho", restaurantName: "turtle tower", craves: 416)
+    let icecream = FeedItem(photo: (UIImage(named: "icecream"))!, itemName: "ice cream", restaurantName: "joe's ice cream", craves: 523)
+    let salad = FeedItem(photo: (UIImage(named: "salad"))!, itemName: "salad", restaurantName: "crepevine", craves: 416)
+    let sushirrito = FeedItem(photo: (UIImage(named: "sushirrito"))!, itemName: " og sushirrito", restaurantName: "sushirrito downtown sf", craves: 863)
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,7 +37,32 @@ class FeedViewController: UITableViewController {
             navigationBar.addSubview(foodarHeader)
 
         }
+        
+        addPhotos()
     }
+    
+    // function to create and append  new post items to the array of existing post objects
+    func createPost(photo: UIImage, itemName: String, restaurantName:String, craves: Int)
+    {
+        /* for customizing later...
+         
+        let newItem = FeedItem(photo: photo, itemName: itemName, restaurantName: restaurantName, craves: craves)
+         
+        photoList.append(newItem)
+        */
+    }
+    
+    // can use addPhotos() method when figuring out how to manually refresh photos later? .append MUST be used inside of a method and called upon in viewDidLoad()
+    func addPhotos() {
+        photoList.append(burger)
+        photoList.append(pho)
+        photoList.append(icecream)
+        photoList.append(salad)
+        photoList.append(sushirrito)
+        
+        //is there a more efficient way of doing this?
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -51,7 +82,7 @@ class FeedViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(
             withIdentifier: "CellItem", for: indexPath)
         
-        cell.imageView?.image = photoList[indexPath.item]
+        cell.imageView?.image = photoList[indexPath.row].photo
         
         return cell
     }
